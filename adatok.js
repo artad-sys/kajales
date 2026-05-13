61e7a493acfd2b76dadbaee41958c26d6ce52609
artad-sys <59153973+artad-sys@users.noreply.github.com>
2026-05-13 04:06:06 +0200

    Add files via upload

---
+++ adatok.js
@@ -0,0 +1,20 @@
+const RESTAURANT_DATA = [
+  {
+    "id": 1778632873850,
+    "name": "kaja 1",
+    "address": "kaja",
+    "open": "H-P 10-20",
+    "phone": "06123455666",
+    "email": "kuki@nuni.com",
+    "menu": "#Előétel\n\npina 1500\nfasz 2000\n\n#Főétel\npina 1500\nfasz 2000"
+  },
+  {
+    "id": 1778636857225,
+    "name": "kettedik",
+    "address": "kettedik",
+    "open": "H-P: 10-12",
+    "phone": "0630111111",
+    "email": "valami@valami.hu",
+    "menu": "#Előétel\n\nvalami - 1500\nde az nagyon - 2000\n\n# Főétel\naz ja - 1000\nbaz jabaz - 2000"
+  }
+];
